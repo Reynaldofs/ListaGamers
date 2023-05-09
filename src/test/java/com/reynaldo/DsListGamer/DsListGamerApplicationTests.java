39047ccd58177465cc86b92086743a4b307d130f
Reynaldofs <sales3373@gmail.com>
2023-05-08 21:50:44 -0300

    inicialização do projeto

---
+++ src/test/java/com/reynaldo/DsListGamer/DsListGamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reynaldo.DsListGamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListGamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
